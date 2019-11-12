08ca300e73fd3fa0389da8598e7d6b23e5a4a776
cofire <yli_666@163.com>
2019-11-12 14:38:12 +0800

    分页查询长度最大为100

--- cofire/common/src/main/java/com/cofire/common/result/ParamItem.java
+++ cofire/common/src/main/java/com/cofire/common/result/ParamItem.java
@@ -9,20 +9,10 @@ package com.cofire.common.result;
  * @param <T>
  */
 public class ParamItem {
-	//停车场页面传入的为50/100
-	private Integer length1;
     // 当前页面
     private Integer page;
     // 页面大小
     private Integer length;
-    // 标志 add 代表新增 mod 代表编辑
-    private String flag;
-    // 创建时间开始时间
-    private String createTimeStr;
-	// 创建时间结束时间
-    private String createTimeEnd;
-    // 用于创建时间组件传入的时间数组
-    private String[] createTimes;
 
     public Integer getPage() {
         return page;
@@ -37,47 +27,9 @@ public class ParamItem {
     }
 
     public void setLength(Integer length) {
+        if (length > 100) {
+            length = 100;
+        }
         this.length = length;
     }
-
-    public String getFlag() {
-        return flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
-
-	public String getCreateTimeStr() {
-		return createTimeStr;
-	}
-
-	public void setCreateTimeStr(String createTimeStr) {
-		this.createTimeStr = createTimeStr;
-	}
-
-	public String getCreateTimeEnd() {
-		return createTimeEnd;
-	}
-
-	public void setCreateTimeEnd(String createTimeEnd) {
-		this.createTimeEnd = createTimeEnd;
-	}
-
-    public String[] getCreateTimes() {
-        return createTimes;
-    }
-
-    public void setCreateTimes(String[] createTimes) {
-        this.createTimes = createTimes;
-    }
-
-	public Integer getLength1() {
-		return length1;
-	}
-
-	public void setLength1(Integer length1) {
-		this.length1 = length1;
-	}
-    
 }
