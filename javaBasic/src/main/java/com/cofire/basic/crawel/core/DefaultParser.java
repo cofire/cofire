073bc32d12b432e54bd36643822cdb702ddc4158
cofire <yli_666@163.com>
2020-02-13 20:47:56 +0800

    添加爬虫方法

--- javaBasic/src/main/java/com/cofire/basic/crawel/core/DefaultParser.java
+++ javaBasic/src/main/java/com/cofire/basic/crawel/core/DefaultParser.java
@@ -2,12 +2,7 @@ package com.cofire.basic.crawel.core;
 
 import com.cofire.basic.crawel.template.TemplateMethod;
 import org.apache.commons.lang3.StringUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -19,51 +14,17 @@ import java.lang.reflect.Method;
 public class DefaultParser {
 
     //    获取元素的值
-    public static String getElementValue(Element element, TemplateMethod templateMethod) {
+    public static <T> String getElementValue(T element, TemplateMethod templateMethod) {
         try {
             if (null == element || null == templateMethod || StringUtils.isEmpty(templateMethod.getName())) {
                 return "";
             }
-            Method method = element.getClass().getDeclaredMethod(templateMethod.getName(), String.class);
+            Method method = getElemetMethod(element, templateMethod);
             //        代表最终方法 返回 string
             if (null == templateMethod.getTemplateMethod() || null == templateMethod.getTemplateMethod().getName()) {
-                return (String) method.invoke(element, templateMethod.getParam());
+                return (String) elementMethodInvoke(method, templateMethod, element);
             } else {
-                if (method.getReturnType() == Element.class) {
-                    return getElementValue((Element) method.invoke(element, templateMethod.getParam()), templateMethod.getTemplateMethod());
-                } else {
-                    return getElementsValue((Elements) method.invoke(element, templateMethod.getParam()),
-                            templateMethod.getTemplateMethod());
-                }
-            }
-        } catch (
-                NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
-    //    获取元素的值
-    public static String getElementsValue(Elements elements, TemplateMethod templateMethod) {
-        try {
-            if (null == elements || null == templateMethod || StringUtils.isEmpty(templateMethod.getName())) {
-                return "";
-            }
-            Method method = elements.getClass().getDeclaredMethod(templateMethod.getName(), String.class);
-            //        代表最终方法 返回 string
-            if (null == templateMethod.getTemplateMethod() || null == templateMethod.getTemplateMethod().getName()) {
-                return (String) method.invoke(elements, templateMethod.getParam());
-            } else {
-                if (method.getReturnType() == Element.class) {
-                    return getElementValue((Element) method.invoke(elements, templateMethod.getParam()), templateMethod.getTemplateMethod());
-                } else {
-                    return getElementsValue((Elements) method.invoke(elements, templateMethod.getParam()),
-                            templateMethod.getTemplateMethod());
-                }
+                return getElementValue(elementMethodInvoke(method, templateMethod, element), templateMethod.getTemplateMethod());
             }
         } catch (
                 NoSuchMethodException e) {
@@ -87,26 +48,11 @@ public class DefaultParser {
         }
     }
 
-    public static void main(String[] args) {
-        try {
-            Document document = Jsoup.connect("http://www.yiibai.com").get();
-            Method select = document.getClass().getSuperclass().getDeclaredMethod("select", String.class);
-            Elements xoxoList = (Elements) select.invoke(document, ".xoxo li");
-            TemplateMethod tem = new TemplateMethod("attr", "href");
-            TemplateMethod templateMethod = new TemplateMethod("getElementsByTag", "a", tem);
-            for (Element xoxo : xoxoList) {
-//                System.out.println(xoxo.getElementsByTag("a").attr("href"));
-//                System.out.println(xoxo.getElementsByTag("a").text());
-//                System.out.println(xoxo.ownText());
-                System.out.println(getElementValue(xoxo, templateMethod));
-            }
-        } catch (
-                IOException | NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+    public static <T> Object elementMethodInvoke(Method method, TemplateMethod templateMethod, T element) throws InvocationTargetException, IllegalAccessException {
+        if (StringUtils.isAllEmpty(templateMethod.getParam())) {
+            return method.invoke(element);
+        } else {
+            return method.invoke(element, templateMethod.getParam());
         }
     }
 }
