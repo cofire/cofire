4372216c2669ac453d02e5c558664632635e68c1
cofire <yli_666@163.com>
2020-03-10 09:49:21 +0800

    添加全局异常处理，添加配置文件加密，配置字符串返回结果集

--- cofire/console/src/main/java/com/cofire/console/common/ControllerAdviceHandler.java
+++ cofire/console/src/main/java/com/cofire/console/common/ControllerAdviceHandler.java
@@ -1,11 +1,13 @@
 package com.cofire.console.common;
 
+import com.cofire.common.constant.CodeEnum;
+import com.cofire.common.result.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.cofire.common.constant.CodeEnum;
-import com.cofire.common.result.Result;
 
 /**
  * @author ly
@@ -18,11 +20,18 @@ import com.cofire.common.result.Result;
 @ResponseBody
 public class ControllerAdviceHandler {
 
-    @ExceptionHandler(Exception.class)
-    public Result exception() {
-        Result result = new Result();
-        result.setCode(CodeEnum.E_500.getCode());
-        result.setMessage(CodeEnum.E_500.getMessage());
-        return result;
+    private final static Logger logger = LoggerFactory.getLogger(ControllerAdviceHandler.class);
+
+    @ExceptionHandler(value = Exception.class)
+    public Result exceptionHandler(Exception e) {
+        if (e instanceof NullPointerException) {
+            logger.error("空指针异常：{}", e.getMessage());
+            return new Result(false, CodeEnum.E_500.getCode(), "服务端出现数据为空异常");
+        } else if (e instanceof DuplicateKeyException) {
+            logger.error("主键冲突异常：{}", e.getMessage());
+            return new Result(false, CodeEnum.E_500.getCode(), "数据库主键冲突异常");
+        }
+        logger.error("系统异常:{}", e.getMessage());
+        return new Result(false, CodeEnum.E_500.getCode(), "系统异常");
     }
 }
